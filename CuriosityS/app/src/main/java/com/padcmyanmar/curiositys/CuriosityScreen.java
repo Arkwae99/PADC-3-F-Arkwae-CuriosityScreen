524433dfae4283f2119da3a29754f36ba9af2365
Arkwae <arkwaeapk99@gmail.com>
2017-12-01 15:06:03 +0630

    no message

---
+++ CuriosityS/app/src/main/java/com/padcmyanmar/curiositys/CuriosityScreen.java
@@ -0,0 +1,13 @@
+package com.padcmyanmar.curiositys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CuriosityScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curiosity_screen);
+    }
+}
